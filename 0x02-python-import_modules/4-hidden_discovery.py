c126bc094883c8b8a50897aac7d9ed57f4ae0666
Kiruubel <kiruukass@gmail.com>
2022-12-01 04:11:11 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 
-
-
 if __name__ == "__main__":
 
-    """Print all names defined by hidden_4 module."""
+    import sys
 
     import hidden_4
 
+    for n in dir(hidden_4):
 
+        if n[:2] != "__":
 
-    names = dir(hidden_4)
-
-    for name in names:
-
-        if name[:2] != "__":
-
-            print(name)
+            print(n)
